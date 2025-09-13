f7c8dfa8cada61be8f517280d730b1f19525d00c
Ana Priscilla F. <web.anapriscilla@gmail.com>
2025-09-13 17:09:25 -0300

    update portfolio

--- portfolio/script/script.js
+++ portfolio/script/script.js
@@ -1,39 +1,27 @@
-const projetos = [
-  {
-    titulo: "Projeto 1",
-    descricao: "Landing page responsiva com HTML e CSS.",
-    tecnologias: "HTML, CSS",
-    imagem: "https://via.placeholder.com/800x500"
-  },
-  {
-    titulo: "Projeto 2",
-    descricao: "Blog em React com integração via API.",
-    tecnologias: "React, JavaScript",
-    imagem: "assets/02-completo.png"
-  },
-  {
-    titulo: "Projeto 3",
-    descricao: "Dashboard com gráficos interativos.",
-    tecnologias: "JavaScript, Chart.js",
-    imagem: "https://via.placeholder.com/800x500"
-  },
-  {
-    titulo: "Projeto 4",
-    descricao: "App de tarefas com salvamento local.",
-    tecnologias: "HTML, JS, LocalStorage",
-    imagem: "https://via.placeholder.com/800x500"
-  }
-];
+ const projetos = [
+      { titulo: "Projeto 1", descricao: "Landing page responsiva com HTML e CSS.", imagem: "https://via.placeholder.com/800x1200" },
+      { titulo: "", descricao: "", imagem: "assets/02-completo.png" },
+      { titulo: "Projeto 3", descricao: "Dashboard com gráficos interativos.", imagem: "https://via.placeholder.com/800x1200" },
+      { titulo: "", descricao: "", imagem: "assets/revenda-oi.png" }
+    ];
 
-function abrirModal(index) {
+    function abrirModal(index) {
   const modal = document.getElementById("modal");
   document.getElementById("modal-titulo").textContent = projetos[index].titulo;
   document.getElementById("modal-descricao").textContent = projetos[index].descricao;
-  document.getElementById("modal-tecnologias").textContent = projetos[index].tecnologias;
   document.getElementById("modal-img").src = projetos[index].imagem;
   modal.style.display = "flex";
+  document.body.style.overflow = "hidden";
+  document.querySelector(".modal-fechar").style.display = "flex"; // mostra botão fechar
 }
 
 function fecharModal() {
   document.getElementById("modal").style.display = "none";
+  document.body.style.overflow = "auto";
+  document.querySelector(".modal-fechar").style.display = "none"; // esconde botão fechar
 }
+
+    function scrollToTopo() {
+      const modalContainer = document.querySelector(".modal-container");
+      modalContainer.scrollTo({ top: 0, behavior: 'smooth' });
+    }
